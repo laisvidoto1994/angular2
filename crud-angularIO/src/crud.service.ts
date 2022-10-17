d20b8fd27c41373f7d9d9df5c4017f5dea08db0f
laisvidoto1994 <lais.vidoto@gmail.com>
2022-10-17 18:07:47 -0300

    atualizacaoServico

--- crud-angularIO/src/crud.service.ts
+++ crud-angularIO/src/crud.service.ts
@@ -3,48 +3,52 @@ import { HttpClient } from "@angular/common/http";
 
 @Injectable()
 export class CrudService {
+  constructor(private http: HttpClient) {}
 
-    constructor(private http: HttpClient) { }
+  configUrl = "http://localhost:3000/produtos";
 
-    configUrl = "http://localhost:3000/produtos";
+  /**********************************/
+  /*  services do json de dados     */
+  /**********************************/
 
-    /* Leitura dos dados */
-    read() {
-        return this.http.get(this.configUrl)
-    }
+  // Leitura dos dados
+  read() {
+    return this.http.get(this.configUrl);
+  }
 
-    /* procurar dos dados */
-    search(id) {
-        return this.http.get(`${this.configUrl}/${id}`)
-    }
+  // procurar dos dados
+  search(id) {
+    return this.http.get(`${this.configUrl}/${id}`);
+  }
 
-    /* Criação dos dados */
-    create(client) {
-        return this.http.post(this.configUrl, client)
-    }
+  // Criação dos dados
+  create(client) {
+    return this.http.post(this.configUrl, client);
+  }
 
-    /* Atualização dos dados do json */
-    update(id, client) {
-        return this.http.put(`${this.configUrl}/${id}`, client);
-    }
+  // Atualização dos dados do json
+  update(id, client) {
+    return this.http.put(`${this.configUrl}/${id}`, client);
+  }
 
-    /* Exclusão dos dados */
-    delete(id) {
-        return this.http.delete(`${this.configUrl}/${id}`)
-    }
+  // Exclusão dos dados
+  delete(id) {
+    return this.http.delete(`${this.configUrl}/${id}`);
+  }
 
-    /**********************************/
-    /*      service de menssagem      */
-    /**********************************/
-    configUrl2 = "http://localhost:3000/menssagens";
 
-    /* Criação dos dados */
-    create2(client) {
-        return this.http.post(this.configUrl2, client)
-    }
+  /**********************************/
+  /*      services de menssagem      */
+  /**********************************/
+  configUrl2 = "http://localhost:3000/menssagens";
 
-    /* Leitura dos dados */
-    read2() {
-        return this.http.get(this.configUrl2)
-    }
+  /* Criação dos dados */
+  create2(client) {
+    return this.http.post(this.configUrl2, client);
+  }
+
+  /* Leitura dos dados */
+  read2() {
+    return this.http.get(this.configUrl2);
+  }
 }
