7293697cdaea21c5f151b97ba2f326e936d4f15f
laisvidoto1994 <lais.vidoto@gmail.com>
2022-10-17 18:15:54 -0300

    atualizacaoTableTS

--- crud-angularIO/src/app/table/table.component.ts
+++ crud-angularIO/src/app/table/table.component.ts
@@ -1,37 +1,38 @@
 import { Component, OnInit } from "@angular/core";
-
 import { CrudService } from "../../crud.service";
 
 @Component({
   selector: "table-component",
   templateUrl: "./table.component.html",
-  styleUrls: ["./table.component.css"]
+  styleUrls: ["./table.component.css"],
 })
-
 export class TableComponent implements OnInit {
-
-  /* lista de produtos que vem do service */
+  // lista de produtos que vem do service
   produtos: any = [];
 
-  /* paginação */
+  // paginação
   filteredTodos: any = [];
   currentPage = 1;
   itemsPerPage = 1;
 
-  /* resultado da busca */
+  // resultado da busca
   resultadoBusca: any = [];
 
-  /* validação de exclusão */
+  // validação de exclusão
   excluirValidacao: boolean = false;
 
-  constructor(public crudService: CrudService) { }
+  constructor(
+    public crudService: CrudService
+    ) {}
 
   ngOnInit() {
-    this.getClients();
+    this.carregarListaProdutos();
   }
 
-  /* botão de busca de dados pelo input */
-  observar(dado) { 
+  /*
+  * botão de busca de dados pelo input
+  */
+  pesquisarProdutos(dado) {
     console.log("digitado-> " + dado.value.nomeHospedagem);
     this.resultadoBusca = [];
 
@@ -41,61 +42,59 @@ export class TableComponent implements OnInit {
       }
     }
     if (this.resultadoBusca == "") {
-      this.resultadoBusca = "Nenhum hotel foi encontrado com esse nome!"
+      this.resultadoBusca = "Nenhum hotel foi encontrado com esse nome!";
     }
   }
 
-
-  /* Mostra TODOS os dados do json */
-  getClients() {
-
-    this.crudService
-      .read().subscribe(
-        data => {
-          this.produtos = data;
-        },
-        error => {
-          console.log(error);
-        }
-      );
+  /*
+  * Mostra TODOS os dados do json
+  */
+  carregarListaProdutos() {
+    this.crudService.read().subscribe(
+      (data) => {
+        this.produtos = data;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
 
-  /* Mostra UM UNICO dado do json */
-  trazerCliente(id, client) {
-
-    this.crudService
-      .search(id).subscribe(
-        data => {
-          this.produtos = data;
-          //   console.log(data);
-        },
-        error => {
-          console.log(error);
-        }
-      );
-
+  /*
+  * Mostra UM UNICO dado do json
+  */
+  detalharProduto(id, client) {
+    this.crudService.search(id).subscribe(
+      (data) => {
+        this.produtos = data;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
 
-  /* exclui dados do json */
-  deleteClient(id, client) {
-
-    this.crudService
-      .delete(id).subscribe(
-        data => {
-          this.excluirValidacao = true;
-          window.location.reload();
-        },
-        error => {
-          console.log("ERROR: " + error)
-        }
-      );
+  /*
+  * exclui dados do json
+  */
+  excluirProduto(id, client) {
+    this.crudService.delete(id).subscribe(
+      (data) => {
+        this.excluirValidacao = true;
+        window.location.reload();
+      },
+      (error) => {
+        console.log("ERROR: " + error);
+      }
+    );
   }
 
-  /* lgica da paginação */
+  /*
+  * logica da paginação
+  */
   pageChanged() {
-    var begin = ((this.currentPage - 1) * this.itemsPerPage);
+    var begin = (this.currentPage - 1) * this.itemsPerPage;
     var end = begin + this.itemsPerPage;
     this.filteredTodos = this.produtos.slice(begin, end);
-  };
-
+  }
 }
