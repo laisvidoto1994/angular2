454afd5b05ee9837448b8f8d74914e058d1494b7
laisvidoto1994 <lais.vidoto@gmail.com>
2022-10-18 17:19:39 -0300

    Update modal.component.ts

--- crud-angularIO/src/app/modal/modal.component.ts
+++ crud-angularIO/src/app/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-modal',
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModalComponent implements OnInit {
 
+  @Input('operacao') operacao: string = '';
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  cadastrar(){
+    this.operacao = 'cadastrar';
+  }
+
+  naoCadastrar(){
+    this.operacao = '';
+  }
 }
